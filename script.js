739b4c2642e6ef8ace04b85ace35bf854590edf0
Kirill Makhnovskiy <script.makhnovskiy@gmail.com>
2022-08-27 00:22:33 +0300

    added hw

---
+++ script.js
@@ -0,0 +1,34 @@
+const store = {
+    name: 'Gym Store',
+    adress: {
+        country: 'USA',
+        city: 'Los Angeles',
+        street: 'West Broadway. 709 W Broadway',
+        houseNumber: 5
+    }
+};
+
+store.clients = '';
+
+store.clients = {
+    'Bohdan Onatskyi':{
+        quantity: 1,
+        bonuses: Infinity
+    },
+    'Oleksii Medvedev':{
+        quantity: 5,
+        bonuses: 89
+    },
+    'Nataliia Kulybina':{
+        quantity: 17,
+        bonuses: 789
+    },
+    'Yuliia Zavitovska':{
+        quantity: 8,
+        bonuses: 45
+    },
+    'Olha Kovalova':{
+        quantity: 100,
+        bonuses: 154
+    }
+}
